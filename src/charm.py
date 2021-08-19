784e9060e13d808c2522a97ab89ec7d77c4c094b
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-19 19:32:41 -0400

    group by topology (#26)
    * group by juju topology
    
    * fix gh action

--- src/charm.py
+++ src/charm.py
@@ -342,7 +342,7 @@ class AlertmanagerCharm(CharmBase):
         default_alertmanager_config = textwrap.dedent(
             """
             route:
-              group_by: ['alertname']
+              group_by: ['juju_application', 'juju_model', 'juju_model_uuid']
               group_wait: 30s
               group_interval: 5m
               repeat_interval: 1h
@@ -351,12 +351,6 @@ class AlertmanagerCharm(CharmBase):
             - name: 'web.hook'
               webhook_configs:
               - url: 'http://127.0.0.1:5001/'
-            inhibit_rules:
-              - source_match:
-                  severity: 'critical'
-                target_match:
-                  severity: 'warning'
-                equal: ['alertname', 'dev', 'instance']
             """
         )
         # config: dict = yaml.safe_load(self.container.pull(self._config_path))
