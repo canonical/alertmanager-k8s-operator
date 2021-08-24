6acc932d9fa4448b1eb96a2dd7160cdab4aec8c0
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-24 09:50:25 -0400

    add docstrings

--- src/config.py
+++ src/config.py
@@ -3,31 +3,50 @@ from typing import Optional
 
 
 class ConfigBase(abc.ABC):
+    """Represents the interface to configuration items coming from `config.yaml` and
+    `juju config`.
+    """
+
     # TODO change to Protocol
     @staticmethod
     @abc.abstractmethod
     def from_dict(data: dict) -> Optional[dict]:
-        ...
+        """Convert raw config data (e.g. :class:`ConfigData`, as in `ops.model.config`) to
+        application-specific configuration format.
+        """
 
     @staticmethod
     @abc.abstractmethod
     def is_valid(data: dict) -> bool:
-        ...
+        """Check that the raw config data (e.g. :class:`ConfigData`, as in `ops.model.config`)
+        comprises a valid application-specific configuration.
+        """
 
 
 class ReceiverBase(ConfigBase):
+    """Represents the interface to alertmanager receiver configuration items coming from
+    `config.yaml` and `juju config`.
+    """
+
     @property
     @abc.abstractmethod
     def name(self):
-        ...
+        """Class property that represents a receiver's name in an alertmanager.yml
+        configuration. The same name is used in routing rules.
+        """
 
     @property
     @abc.abstractmethod
     def _section_name(self):
-        ...
+        """Class property that represents a receiver's section name in an alertmanager.yml
+        configuration. This is usually "<name>_configs".
+        """
 
     @classmethod
     def from_dict(cls, data: dict) -> Optional[dict]:
+        """A generic converter, common to all alertmanager receivers, that formats config data
+        into yaml data.
+        """
         if not cls.is_valid(data):
             return None
 
@@ -35,6 +54,8 @@ class ReceiverBase(ConfigBase):
 
 
 class PagerdutyConfig(ReceiverBase):
+    """Utility class for converting config data into a valid pagerduty yaml section."""
+
     name = "pagerduty"
     _section_name = "pagerduty_configs"
 
@@ -47,6 +68,8 @@ class PagerdutyConfig(ReceiverBase):
 
 
 class PushoverConfig(ReceiverBase):
+    """Utility class for converting config data into a valid pushover yaml section."""
+
     name = "pushover"
     _section_name = "pushover_configs"
 
@@ -60,6 +83,8 @@ class PushoverConfig(ReceiverBase):
 
 
 class WebhookConfig(ReceiverBase):
+    """Utility class for converting config data into a valid webhook yaml section."""
+
     name = "webhook"
     _section_name = "webhook_configs"
 
