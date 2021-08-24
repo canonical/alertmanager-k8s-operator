f1b7bb66ab4fc4c379be51dea5b1476882fe31b1
sed-i <82407168+sed-i@users.noreply.github.com>
2021-08-24 18:35:03 -0400

    clean up api client

--- src/alertmanager_client.py
+++ src/alertmanager_client.py
@@ -7,7 +7,7 @@ import logging
 import urllib.error
 import urllib.parse
 import urllib.request
-from typing import List, Optional
+from typing import Optional
 
 logger = logging.getLogger(__name__)
 
@@ -27,21 +27,15 @@ class Alertmanager:
           True if reload succeeded (returned 200 OK); False otherwise.
         """
         url = urllib.parse.urljoin(self.base_url, "/-/reload")
-        try:
-            response = urllib.request.urlopen(url, data=None, timeout=self.timeout)
-            logger.debug("config reload via %s: %d %s", url, response.code, response.reason)
-            return response.code == 200 and response.reason == "OK"
-        except (ValueError, urllib.error.HTTPError) as e:
-            logger.debug("config reload error via %s: %s", url, str(e))
-            return False
+        return bool(self._get(url, timeout=self.timeout))
 
     @staticmethod
-    def _get(url: str, timeout) -> Optional[dict]:
+    def _get(url: str, timeout) -> Optional[str]:
         """Send a GET request with a timeout"""
         try:
             response = urllib.request.urlopen(url, data=None, timeout=timeout)
-            if response.code == 200:
-                text = json.loads(response.read())
+            if response.code == 200 and response.reason == "OK":
+                text = response.read()
             else:
                 text = None
         except (ValueError, urllib.error.HTTPError, urllib.error.URLError):
@@ -51,20 +45,7 @@ class Alertmanager:
     def status(self) -> Optional[dict]:
         """Obtain status information from the alertmanager server."""
         url = urllib.parse.urljoin(self.base_url, "/api/v2/status")
-        return self._get(url, timeout=self.timeout)
-
-    def silences(self, state: str = None) -> Optional[List[dict]]:
-        """Obtain information on silences from the alertmanager server."""
-        url = urllib.parse.urljoin(self.base_url, "/api/v2/silences")
-        silences = self._get(url, timeout=self.timeout)
-
-        # if GET failed or user did not provide a state to filter by, return as-is (possibly None);
-        # else filter by state
-        return (
-            None
-            if silences is None or state is None
-            else [s for s in silences if s.get("status") and s["status"].get("state") == state]
-        )
+        return json.loads(response) if (response := self._get(url, timeout=self.timeout)) else None
 
     @property
     def version(self) -> str:
