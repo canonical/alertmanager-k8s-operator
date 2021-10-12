b630d6e37119bc081dd05164df8bfeea9c8ed205
Leon <82407168+sed-i@users.noreply.github.com>
2021-10-12 13:02:39 -0400

    Upkeep (#39)
    * use codespell
    
    * minor doc fix
    
    * add codeql generic workflow
    
    * add on_push to ci ghwf
    
    * pyproject: drop [project]; add copyright notice
    
    * add default to relation name
    
    * validate relation name and role using abstractmethod
    
    * validate relation name and role without abstractmethod
    
    * more informative ValueError message
    
    * validate interface name against hardcoded literal
    
    * add pip-check-reqs to the static tox env
    
    * add docstring
    
    * move interface name to module scope
    
    * minor tox fixes
    
    * more codespell

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,11 +1,14 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+"""Helper functions for writing tests."""
+
 import logging
 
 log = logging.getLogger(__name__)
 
 
 async def get_unit_address(ops_test, app_name: str, unit_num: int) -> str:
+    """Get private address of a unit."""
     status = await ops_test.model.get_status()  # noqa: F821
     return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
