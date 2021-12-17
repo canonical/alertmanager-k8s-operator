c97e76b2185bef682166c06f61ec09011b8d9c54
sed-i <82407168+sed-i@users.noreply.github.com>
2021-12-17 14:34:44 +0000

    dropping the global for now so we can merge

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -7,14 +7,10 @@ from pathlib import Path
 import pytest
 from pytest_operator.plugin import OpsTest
 
-_path_to_built_charm = None
-
 
 @pytest.fixture(scope="module")
 async def charm_under_test(ops_test: OpsTest) -> Path:
     """Charm used for integration testing."""
-    global _path_to_built_charm
-    if _path_to_built_charm is None:
-        _path_to_built_charm = await ops_test.build_charm(".")
+    path_to_built_charm = await ops_test.build_charm(".")
 
-    return _path_to_built_charm
+    return path_to_built_charm
