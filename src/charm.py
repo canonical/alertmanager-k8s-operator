cd808b86fb7f42e1040abc19b3de58c94fd6d1f9
Jose C. Massón <jose.masson@canonical.com>
2022-06-24 15:27:52 -0300

    grafana-dashboard relation added

--- src/charm.py
+++ src/charm.py
@@ -11,6 +11,7 @@ from typing import List, cast
 
 import yaml
 from charms.alertmanager_k8s.v0.alertmanager_dispatch import AlertmanagerProvider
+from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
 from charms.karma_k8s.v0.karma_dashboard import KarmaProvider
 from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
@@ -88,6 +89,7 @@ class AlertmanagerCharm(CharmBase):
             relation_name="self-metrics-endpoint",
             jobs=[{"static_configs": [{"targets": [f"*:{self._api_port}"]}]}],
         )
+        self.grafana_dashboard_provider = GrafanaDashboardProvider(charm=self)
 
         self.container = self.unit.get_container(self._container_name)
 
