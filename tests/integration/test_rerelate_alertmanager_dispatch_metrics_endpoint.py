ac424066a1a247de23081219af256ad1a0926888
Jose C. Massón <jose.masson@canonical.com>
2022-06-24 22:50:16 -0300

    Rename itest and test new relation

--- tests/integration/test_rerelate_alertmanager_dispatch_metrics_endpoint.py
+++ tests/integration/test_rerelate_alertmanager_dispatch_metrics_endpoint.py
@@ -4,7 +4,8 @@
 
 """This test module tests alertmanager response to related apps being removed and re-related.
 
-1. Deploy the charm under test and a related app, relate them and wait for them to become idle.
+1. Deploy the charm under test and a related app (Promethes) relate them using
+   `alertmanager_dispatch` and `prometheus_scrape` interfaces and wait for them to become idle.
 2. Remove the relation.
 3. Re-add the relation.
 4. Remove the related application.
@@ -43,10 +44,18 @@ async def test_build_and_deploy(ops_test: OpsTest, charm_under_test):
 
     assert await is_alertmanager_up(ops_test, app_name)
 
+    await ops_test.model.add_relation(app_name, f"{related_app}:metrics-endpoint")
+    await ops_test.model.wait_for_idle(apps=[app_name, related_app], status="active", timeout=1000)
+
+    assert await is_alertmanager_up(ops_test, app_name)
+
 
 @pytest.mark.abort_on_fail
 async def test_remove_relation(ops_test: OpsTest):
     await ops_test.model.applications[app_name].remove_relation("alerting", related_app)
+    await ops_test.model.applications[app_name].remove_relation(
+        "self-metrics-endpoint", related_app
+    )
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
     assert await is_alertmanager_up(ops_test, app_name)
 
@@ -54,6 +63,10 @@ async def test_remove_relation(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_rerelate(ops_test: OpsTest):
     await ops_test.model.add_relation(app_name, f"{related_app}:alertmanager")
+    await ops_test.model.wait_for_idle(apps=[app_name, related_app], status="active", timeout=1000)
+    assert await is_alertmanager_up(ops_test, app_name)
+
+    await ops_test.model.add_relation(app_name, f"{related_app}:metrics-endpoint")
     await ops_test.model.wait_for_idle(apps=[app_name, related_app], status="active", timeout=1000)
     assert await is_alertmanager_up(ops_test, app_name)
 
@@ -72,5 +85,9 @@ async def test_remove_related_app(ops_test: OpsTest):
 async def test_rerelate_app(ops_test: OpsTest):
     await ops_test.model.deploy("ch:prometheus-k8s", application_name=related_app, channel="edge")
     await ops_test.model.add_relation(app_name, f"{related_app}:alertmanager")
+    await ops_test.model.wait_for_idle(apps=[app_name, related_app], status="active", timeout=1000)
+    assert await is_alertmanager_up(ops_test, app_name)
+
+    await ops_test.model.add_relation(app_name, f"{related_app}:metrics-endpoint")
     await ops_test.model.wait_for_idle(apps=[app_name, related_app], status="active", timeout=1000)
     assert await is_alertmanager_up(ops_test, app_name)
