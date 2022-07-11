66d489e706d73e17a609b5d227a2d99828647ce7
Balbir Thomas <balbir.thomas@canonical.com>
2022-07-11 13:23:57 +0100

    Use kubectl to send a sigterm signal to Alertmanager
    This commit uses kubectl to send a SIGTERM signal to alertmanager
    during the integration test for persistence of silences. This
    is temporarily necessary because this bug
    https://github.com/canonical/pebble/issues/122 .

--- tests/integration/test_persistence.py
+++ tests/integration/test_persistence.py
@@ -43,6 +43,43 @@ async def test_silences_persist_across_upgrades(ops_test: OpsTest, charm_under_t
     silences_before = alertmanager.get_silences()
     assert len(silences_before)
 
+    # Use kubectl to send a SIGTERM signal to Alertmanager so that data is flushed to disk.
+    # This step should not be necessary once the bug in the following issue ticket is fixed
+    # https://github.com/canonical/pebble/issues/122
+    # FIXME: remove the following kubectl commands once the above bug is fixed
+    pod_name = f"{app_name}-0"
+    container_name = "alertmanager"
+    sg_cmd = [
+        "sg",
+        "microk8s",
+        "-c",
+    ]
+    kubectl_cmd = [
+        "microk8s.kubectl",
+        "-n",
+        ops_test.model_name,
+        "exec",
+        pod_name,
+        "-c",
+        container_name,
+        "--",
+    ]
+    # find pid of alertmanager
+    pid_cmd = ["pidof", "alertmanager"]
+    cmd = sg_cmd + [" ".join(kubectl_cmd + pid_cmd)]
+    retcode, alertmanager_pid, stderr = await ops_test.run(*cmd)
+    assert retcode == 0, f"kubectl failed: {(stderr or alertmanager_pid).strip()}"
+    # use pid of alertmanager to send it a SIGTERM signal using kubectl
+    term_cmd = ["kill", "-s", "TERM", alertmanager_pid]
+    cmd = sg_cmd + [" ".join(kubectl_cmd + term_cmd)]
+    logger.debug("Sending SIGTERM to Alertmanager")
+    retcode, stdout, stderr = await ops_test.run(*cmd)
+    assert retcode == 0, f"kubectl failed: {(stderr or stdout).strip()}"
+    logger.debug(stdout)
+    await ops_test.model.block_until(lambda: len(ops_test.model.applications[app_name].units) > 0)
+    await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
+    assert await is_alertmanager_up(ops_test, app_name)
+
     # upgrade alertmanger using charm built locally
     logger.info("upgrade deployed charm with local charm %s", charm_under_test)
     await ops_test.model.applications[app_name].refresh(path=charm_under_test, resources=resources)
