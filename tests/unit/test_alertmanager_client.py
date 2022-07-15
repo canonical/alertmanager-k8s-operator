0a6ec55039d76878f692a360daaf6cb7bc17a6b2
Balbir Thomas <balbir.thomas@canonical.com>
2022-07-15 11:55:44 +0100

    Integration test for persistence of silences (#78)
    * Enhance alertmanager client with silences API
    
    This commit enhances the Alertmanager client object to add support
    for more methods such as setting, getting and deleting silences,
    in addition to getting current list of alerts.
    
    * An integration test for persistence of silences
    
    This commit implements and integration test to check for the
    persistence of silences. The test deploys Alertmanager from
    charmhub, then sets a silence, then upgrades Alertmanger using
    a locally built charm, and finally checks if the silence still
    persists.
    
    * Use kubectl to send a sigterm signal to Alertmanager
    
    This commit uses kubectl to send a SIGTERM signal to alertmanager
    during the integration test for persistence of silences. This
    is temporarily necessary because this bug
    https://github.com/canonical/pebble/issues/122 .
    
    * Added unit tests for enhanced alertmanager client
    
    This commit adds a few more unit tests for the recent
    enhancements made to the alertmanager client.

--- tests/unit/test_alertmanager_client.py
+++ tests/unit/test_alertmanager_client.py
@@ -4,7 +4,7 @@
 
 import json
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from unittest.mock import patch
 
 from alertmanager_client import Alertmanager, AlertmanagerBadResponse
@@ -78,5 +78,72 @@ class TestAlertmanagerAPIClient(unittest.TestCase):
             }
         ]
         status = self.api.set_alerts(alerts)
+        urlopen_mock.assert_called()
+        self.assertEqual(status, msg)
+
+    @patch("alertmanager_client.urllib.request.urlopen")
+    def test_available_alerts_are_returned(self, urlopen_mock):
+        fake_alerts = [
+            {
+                "labels": {"name": "fake-alert"},
+                "startsAt": datetime.now().isoformat("T"),
+            }
+        ]
+        urlopen_mock.return_value.read = lambda: json.dumps(fake_alerts)
+        urlopen_mock.return_value.code = 200
+        urlopen_mock.return_value.reason = "OK"
+
+        alerts = self.api.get_alerts()
+        self.assertListEqual(alerts, fake_alerts)
+
+    @patch("alertmanager_client.urllib.request.urlopen")
+    def test_silences_can_be_set(self, urlopen_mock):
+        msg = "HTTP 200 OK"
+        urlopen_mock.return_value = msg
+        matchers = [
+            {
+                "name": "alertname",
+                "value": "fake-alert",
+                "isRegex": False,
+            }
+        ]
+        silence_start = datetime.now(timezone.utc)
+        silence_end = silence_start + timedelta(minutes=60)
+        status = self.api.set_silences(
+            matchers=matchers, start_time=silence_start, end_time=silence_end
+        )
+        urlopen_mock.assert_called()
+        self.assertEqual(status, msg)
+
+    @patch("alertmanager_client.urllib.request.urlopen")
+    def test_available_silences_are_returned(self, urlopen_mock):
+        fake_silences = [
+            {
+                "id": "fake-silencer",
+                "status": {"state": "active"},
+                "startsAt": datetime.now().isoformat("T"),
+                "endsAt": (datetime.now() + timedelta(minutes=60)).isoformat("T"),
+                "matchers": [
+                    {
+                        "name": "alertname",
+                        "value": "fake-alert",
+                        "isRegex": False,
+                    }
+                ],
+            }
+        ]
+        urlopen_mock.return_value.read = lambda: json.dumps(fake_silences)
+        urlopen_mock.return_value.code = 200
+        urlopen_mock.return_value.reason = "OK"
+
+        alerts = self.api.get_silences()
+        self.assertListEqual(alerts, fake_silences)
+
+    @patch("alertmanager_client.urllib.request.urlopen")
+    def test_silences_can_be_deleted(self, urlopen_mock):
+        msg = "HTTP 200 OK"
+        urlopen_mock.return_value = msg
+
+        status = self.api.delete_silence("fake-id")
         urlopen_mock.assert_called()
         self.assertEqual(status, msg)
