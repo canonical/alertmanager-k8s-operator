093f3b24418bc48e014e6b640926260d714c289a
sed-i <82407168+sed-i@users.noreply.github.com>
2022-08-03 12:43:14 -0400

    Import ResourceRequirements from the charm lib

--- src/charm.py
+++ src/charm.py
@@ -18,12 +18,12 @@ from charms.karma_k8s.v0.karma_dashboard import KarmaProvider
 from charms.observability_libs.v0.kubernetes_compute_resources_patch import (
     K8sResourcePatchFailedEvent,
     KubernetesComputeResourcesPatch,
+    ResourceRequirements,
     adjust_resource_requirements,
 )
 from charms.observability_libs.v0.kubernetes_service_patch import KubernetesServicePatch
 from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 from charms.traefik_k8s.v1.ingress import IngressPerAppRequirer
-from lightkube.models.core_v1 import ResourceRequirements
 from ops.charm import ActionEvent, CharmBase
 from ops.framework import StoredState
 from ops.main import main
