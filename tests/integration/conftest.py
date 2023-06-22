6ca05cd1a890a95926de02d9738b6666fd432afe
sed-i <82407168+sed-i@users.noreply.github.com>
2023-06-21 21:19:43 -0400

    Add itest

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,19 +2,62 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import functools
+import logging
 import socket
+from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 
 import pytest
 from pytest_operator.plugin import OpsTest
 
 PYTEST_HTTP_SERVER_PORT = 8000
+logger = logging.getLogger(__name__)
+
+
+class Store(defaultdict):
+    def __init__(self):
+        super(Store, self).__init__(Store)
+
+    def __getattr__(self, key):
+        """Override __getattr__ so dot syntax works on keys."""
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        """Override __setattr__ so dot syntax works on keys."""
+        self[key] = value
+
+
+store = Store()
+
+
+def timed_memoizer(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        fname = func.__qualname__
+        logger.info("Started: %s" % fname)
+        start_time = datetime.now()
+        if fname in store.keys():
+            ret = store[fname]
+        else:
+            logger.info("Return for {} not cached".format(fname))
+            ret = await func(*args, **kwargs)
+            store[fname] = ret
+        logger.info("Finished: {} in: {} seconds".format(fname, datetime.now() - start_time))
+        return ret
+
+    return wrapper
 
 
 @pytest.fixture(scope="module")
+@timed_memoizer
 async def charm_under_test(ops_test: OpsTest) -> Path:
     """Charm used for integration testing."""
-    path_to_built_charm = await ops_test.build_charm(".")
+    path_to_built_charm = await ops_test.build_charm(".", verbosity="debug")
 
     return path_to_built_charm
 
@@ -32,3 +75,18 @@ def httpserver_listen_address():
     finally:
         s.close()
     return local_ip_address, PYTEST_HTTP_SERVER_PORT
+
+
+@pytest.fixture(autouse=True, scope="module")
+async def setup_env(ops_test: OpsTest):
+    # Prevent "update-status" from interfering with the test:
+    # - if fired "too quickly", traefik will flip between active/idle and maintenance;
+    # - make sure charm code does not rely on update-status for correct operation.
+    await ops_test.model.set_config(
+        {"update-status-hook-interval": "60m", "logging-config": "<root>=WARNING; unit=DEBUG"}
+    )
+
+
+@pytest.fixture(scope="module")
+def temp_dir(tmp_path_factory):
+    return tmp_path_factory.mktemp("data")
---
+++ tests/integration/test_tls_web.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# Copyright 2023 Ubuntu
+# See LICENSE file for licensing details.
+
+import logging
+from pathlib import Path
+from textwrap import dedent
+from types import SimpleNamespace
+
+import pytest
+import yaml
+from helpers import curl, deploy_literal_bundle, get_unit_address
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+am = SimpleNamespace(name="am", scale=2, hostname="alertmanager.local")
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest, charm_under_test):
+    """Deploy 2 alertmanager units, related to a local CA."""
+    test_bundle = dedent(
+        f"""
+        ---
+        bundle: kubernetes
+        applications:
+          {am.name}:
+            charm: {charm_under_test}
+            series: focal
+            scale: {am.scale}
+            trust: true
+            resources:
+              alertmanager-image: {METADATA["resources"]["alertmanager-image"]["upstream-source"]}
+            options:
+              web_external_url: https://{am.hostname}
+          ca:
+            charm: self-signed-certificates
+            channel: edge
+            scale: 1
+        relations:
+        - [am:certificates, ca:certificates]
+        """
+    )
+
+    # Deploy the charm and wait for active/idle status
+    await deploy_literal_bundle(ops_test, test_bundle)  # See appendix below
+    await ops_test.model.wait_for_idle(
+        status="active", raise_on_error=False, timeout=600, idle_period=30
+    )
+    await ops_test.model.wait_for_idle(status="active")
+
+
+@pytest.mark.abort_on_fail
+async def test_tls_files_created(ops_test: OpsTest):
+    """Make sure charm code created web-config, cert and key files."""
+    # juju ssh --container alertmanager am/0 ls /etc/alertmanager/
+    config_path = "/etc/alertmanager/"
+    for i in range(am.scale):
+        unit_name = f"{am.name}/{i}"
+        rc, stdout, stderr = await ops_test.juju(
+            "ssh", "--container", "alertmanager", unit_name, "ls", f"{config_path}"
+        )
+        logger.info("%s: contents of %s: %s", unit_name, config_path, stdout or stderr)
+
+
+@pytest.mark.abort_on_fail
+async def test_server_cert(ops_test: OpsTest):
+    """Inspect server cert and confirm `X509v3 Subject Alternative Name` field is as expected."""
+    # echo \
+    #   | openssl s_client -showcerts -servername $IPADDR:9093 -connect $IPADDR:9093 2>/dev/null \
+    #   | openssl x509 -inform pem -noout -text
+    am_ip_addrs = [await get_unit_address(ops_test, am.name, i) for i in range(am.scale)]
+    for am_ip in am_ip_addrs:
+        cmd = [
+            "sh",
+            "-c",
+            f"echo | openssl s_client -showcerts -servername {am_ip}:9093 -connect {am_ip}:9093 2>/dev/null | openssl x509 -inform pem -noout -text",
+        ]
+        retcode, stdout, stderr = await ops_test.run(*cmd)
+        assert am.hostname in stdout
+
+
+@pytest.mark.abort_on_fail
+async def test_https_reachable(ops_test: OpsTest, temp_dir):
+    """Make sure alertmanager's https endpoint is reachable using curl and ca cert."""
+    for i in range(am.scale):
+        unit_name = f"{am.name}/{i}"
+        # Save CA cert locally
+        # juju show-unit am/0 --format yaml | yq '.am/0."relation-info"[0]."local-unit".data.ca' > /tmp/cacert.pem
+        cmd = [
+            "sh",
+            "-c",
+            f'juju show-unit {unit_name} --format yaml | yq \'.{unit_name}."relation-info"[0]."local-unit".data.ca\'',
+        ]
+        retcode, stdout, stderr = await ops_test.run(*cmd)
+        cert = stdout
+        cert_path = temp_dir / "local.cert"
+        with open(cert_path, "wt") as f:
+            f.writelines(cert)
+
+        # Confirm alertmanager TLS endpoint reachable
+        # curl --fail-with-body --capath /tmp --cacert /tmp/cacert.pem https://alertmanager.local:9093/-/ready
+        response = await curl(
+            ops_test,
+            cert_dir=temp_dir,
+            cert_path=cert_path,
+            ip_addr=await get_unit_address(ops_test, am.name, i),
+            mock_url=f"https://{am.hostname}:9093/-/ready",
+        )
+        assert "OK" in response
