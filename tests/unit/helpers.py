e403a02966b45e7f5cc520520e7287c9d6948be5
James Garner <james.garner@canonical.com>
2025-04-17 10:25:24 +1200

    tests: migrate Context.run argument

--- tests/unit/helpers.py
+++ tests/unit/helpers.py
@@ -52,26 +52,26 @@ def begin_with_initial_hooks_isolated(context: Context, *, leader: bool = True) 
     state = State(config={"config_file": ""}, containers=[container])
     peer_rel = PeerRelation("replicas")
 
-    state = context.run("install", state)
+    state = context.run(context.on.install(), state)
 
     state = state.replace(relations=[peer_rel])
     state = context.run(peer_rel.created_event, state)
 
     if leader:
         state = state.replace(leader=True)
-        state = context.run("leader-elected", state)
+        state = context.run(context.on.leader_elected(), state)
     else:
         state = state.replace(leader=False)
-        state = context.run("leader-settings-changed", state)
+        state = context.run(context.on.leader_settings_changed(), state)
 
-    state = context.run("config-changed", state)
+    state = context.run(context.on.config_changed(), state)
 
     # state = state.with_can_connect("alertmanger")
     container = container.replace(can_connect=True)
     state = state.replace(containers=[container])
     state = context.run(container.pebble_ready_event, state)
 
-    state = context.run("start", state)
+    state = context.run(context.on.start(), state)
 
     return state
 
