35f83bc30eccfc6d2b3813318691562f7b436623
Michael Thamm <mike.thamm@canonical.com>
2026-03-06 15:00:02 -0500

    fix: Split TF endpoints output to requires/provides

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,20 +2,22 @@ output "app_name" {
   value = juju_application.alertmanager.name
 }
 
-output "endpoints" {
+output "provides" {
   value = {
-    # Requires
-    catalogue            = "catalogue",
-    certificates         = "certificates",
-    ingress              = "ingress",
-    tracing              = "tracing",
-    remote_configuration = "remote-configuration"
-
-    # Provides
     alerting              = "alerting"
     karma_dashboard       = "karma-dashboard"
     self_metrics_endpoint = "self-metrics-endpoint"
     grafana_dashboard     = "grafana-dashboard"
     grafana_source        = "grafana-source"
+  }
+}
+
+output "requires" {
+  value = {
+    catalogue            = "catalogue",
+    certificates         = "certificates",
+    ingress              = "ingress",
+    tracing              = "tracing",
+    remote_configuration = "remote-configuration"
   }
 }
