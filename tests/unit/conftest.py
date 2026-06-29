8bde582e46d68a0b113fc6b54a153cc83c09392a
Michael Dmitry <33381599+michaeldmitry@users.noreply.github.com>
2026-06-29 15:16:07 +0300

    fix: constant service restarts (#439)
    * fix: constant service restarts
    
    * fix: lint + tests
    
    * fix: lint
    
    * fix: lint

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,11 @@
+from contextlib import ExitStack
 from unittest.mock import patch
 
 import pytest
 from charms.tempo_coordinator_k8s.v0.charm_tracing import charm_tracing_disabled
 from ops.testing import Context
 
-from src.alertmanager import WorkloadManager
+from alertmanager import WorkloadManager
 from src.charm import AlertmanagerCharm
 
 
@@ -29,14 +30,26 @@ def tautology(*_, **__) -> bool:
 
 @pytest.fixture(autouse=True)
 def alertmanager_charm():
-    with patch("lightkube.core.client.GenericSyncClient"), patch.multiple(
-        "charm.KubernetesComputeResourcesPatch",
-        _namespace="test-namespace",
-        _patch=tautology,
-        is_ready=tautology,
-    ), patch.object(WorkloadManager, "check_config", lambda *a, **kw: ("ok", "")), patch.object(
-        WorkloadManager, "_alertmanager_version", property(lambda *_: "0.0.0")
-    ), patch("subprocess.run"):
+    with ExitStack() as stack:
+        stack.enter_context(patch("lightkube.core.client.GenericSyncClient"))
+        stack.enter_context(
+            patch.multiple(
+                "charm.KubernetesComputeResourcesPatch",
+                _namespace="test-namespace",
+                _patch=tautology,
+                is_ready=tautology,
+            )
+        )
+        stack.enter_context(
+            patch.object(WorkloadManager, "check_config", lambda *a, **kw: ("ok", ""))
+        )
+        stack.enter_context(patch.object(WorkloadManager, "reload", lambda *a, **kw: None))
+        stack.enter_context(
+            patch.object(
+                WorkloadManager, "_alertmanager_version", property(lambda *_: "0.0.0")
+            )
+        )
+        stack.enter_context(patch("subprocess.run"))
         yield AlertmanagerCharm
 
 
