b6eff8b71fbbcc73b1d762b5cacb9ee2aa010a97
Michael Thamm <mike.thamm@canonical.com>
2026-07-01 16:10:34 -0400

    fix: TF channel validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,8 @@ variable "channel" {
   type        = string
 
   validation {
-    condition     = startswith(var.channel, "dev/")
-    error_message = "The track of the channel must be 'dev/'. e.g. 'dev/edge'."
+    condition     = startswith(var.channel, "0.31/")
+    error_message = "The track of the channel must be '0.31/'. e.g. '0.31/edge'."
   }
 }
 
